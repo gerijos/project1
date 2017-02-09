bcd8ac847816532d573462fa501d3f1a17c4045c
Yeraldiny Jose Alonzo <yjose@csumb.edu>
2017-02-09 09:51:57 +0000

    changest othe whole code

--- proj1.py
+++ proj1.py
@@ -1,73 +1,60 @@
 from PIL import Image
-import os.path
-
 
 #path to directory containing images
-path = "/home/ubuntu/workspace/Project1/Project1Images"
-im = Image.open("/home/ubuntu/workspace/Project1Images/1.png")
-Image.show()
+pics = Image.open("./Project1Images/1.png")
+pics = Image.open("./Project1Images/2.png")
+pics = Image.open("./Project1Images/3.png")
+pics = Image.open("./Project1Images/4.png")
+pics = Image.open("./Project1Images/5.png")
+pics = Image.open("./Project1Images/6.png")
+pics = Image.open("./Project1Images/7.png")
+pics = Image.open("./Project1Images/8.png")
+pics = Image.open("./Project1Images/9.png")
+
+def medianOdd(imageList):
+    median = 0
+    #store list length in the variable listLength
+    listLength = len(imageList)
+    #sort the list
+    sortedValues = sorted(imageList)
+    #Location of middle value. Substract one because of zero index
+    sortedValues = sorted(imageList)
+    middleIndex = ((listLength + 1)/2)-1
+    #return the object located that index
+    return sortedValues[middleIndex]
+
 #Creates an image list
-imgList = [] 
+imList = [] 
 
-#for i in range(9):
- #   imgList.append(Image.open("Project1Images/" + str(i+1) + ".png")))
+#to get the width and height of an image
+width = imList[0].size
+height = imList[1].size
 
-#makePicture creates a picture object
+redPixel = []
+greenPixel = []
+bluePixel = []
 
-imgList = Image.open('')
+myRed, myGreen, myBlue = imList[6].getpixel((2,3))
 
-#########
-#nested loop that goes through each row and coloum of each image
-for x in range(0,width):
+print(myRed, myGreen, myBlue)
+
+#creates new image with the given mode and size -> size is a 2-tuble ordered list
+imageNew = (Image.new('RGB',width, height, 'white'))
+
+#nested for loop that goes across each column and row
+for x in range (0,width):
     for y in range(0,height):
-        redPixel = []
-        greenPixel = []
-        bluePixel = []
-        
         #for loop to access pixels of each image
         for i in range(0,9):
-            #"append" puts together each pixel
-            redPixel.append(getRed(getPixel(pics[i],x,y)))
-            greenPixel.append(getGreen(getPixel(pics[i],x,y)))
-            bluePixel.append(getBlue(getPixel(pics[i],x,y)))
+            redPixel.append(getmyRed(getpixel(pics[i](x,y)))
+            greenPixel.append(getmyGreen(myImage.getpixel((x,y)))
+            bluePixel.append(getmyBlue(myImage.getpixel((x,y)))
+           
             
-            #list is sorted for a list with an odd number of items -> middle value is "median"
-            redPixel = sorted(redPixel)
-            greenPixel = sorted(greenPixel)
-            bluePixel = sorted(bluePixel)
-            
-            #assigns the empty pixels to newImage
-            newImagePixel = getPixel(newImagePixel,x,y)
-            
-            #median equation -> where position m is equal to the number of pictures (9) -> index starts at 0
-            m = ((9+1)/2)
-            
-            #assigns the RGB pixels value from RGBPixel[4] -> expression towards set
-            setRed(newImagePixel,redPixel[m])
-            setGreen(newImaePixel,greenPixel[m])
-            setBlue(newImaePixel,greenPixel[m])
-            
-            #shows new image with the person removed
-            show(newImaePixel)
-############            
-
-#new image to put average pixels -> this gives the dimensions equal to that of the pixels of the image(matrix)
-newImageGrayScale = emptyImage(width,height)
-
-for x in range(0, height):
-    redPixel = []
-    greenPixel = []
-    bluePixel = []
-    
-    for i in range(0,9):
-        redPixel.append(getRed(getPixel(pics[i],x,y))
-        greenPixel.append(getGreen(getPixel(pics[i],x,y))
-        bluePixel.append(getBlue(getPixel(pics[i],x,y))
         
-        redPixel = sorted(redPixel)
-        greenPixel = sorted(greenPixel)
-        bluePixel = sorted(greenPixel)
         
-        m = ((9+1)/2)
         
-        redPixel =
+            
+            
+            
+            
